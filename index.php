c6e4af67f1ea80d0557ec089caa1162061bbacf9
Darmanin96 <darmanindaniel@hotmail.es>
2025-10-15 17:59:27 +0100

    Usar ngrox en la api

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: https://unfurbished-elissa-dcollet.ngrok-free.dev");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require_once __DIR__ . '/config/conectar.php';
 require_once __DIR__ . '/controllers/DatosControllers.php';
 
